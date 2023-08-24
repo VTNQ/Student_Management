3c99d0302f29a63a030d47daa4343c64e4c44571
VTNQ <votannamquoc@gmail.com>
2023-08-24 13:54:20 +0700

    VTNQ

--- Student_Management_Admin/src/module-info.java
+++ Student_Management_Admin/src/module-info.java
@@ -1,5 +1,6 @@
 module Student_Management_Admin {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
--- Student_Management_Student/src/module-info.java
+++ Student_Management_Student/src/module-info.java
@@ -1,5 +1,6 @@
 module Student_Management_Student {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
