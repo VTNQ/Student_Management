a755bdf7379a45985ebda452fe1bd3ef8240e2e1
VTNQ <votannamquoc@gmail.com>
2023-08-26 10:58:41 +0700

    create student

---
+++ Student_Management_Student/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.team_fortune.student_management_student {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.team_fortune.student_management_student to javafx.fxml;
+    exports com.team_fortune.student_management_student;
+}
