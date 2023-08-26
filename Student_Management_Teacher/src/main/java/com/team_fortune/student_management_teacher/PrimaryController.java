a57fb07714f60665eeaddcbd2659a2df9de7f16c
VTNQ <votannamquoc@gmail.com>
2023-08-26 10:58:58 +0700

    create teacher

---
+++ Student_Management_Teacher/src/main/java/com/team_fortune/student_management_teacher/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.team_fortune.student_management_teacher;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
